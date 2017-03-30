dd5b0fd0c384c658711afff1626219a7d08056f1
richard schneiderman <schneiderman.richard@yahoo.com>
2017-03-30 18:07:29 -0400

    cleanup

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 class HelloWorld extends React.Component {
     render() {
-        return <h1>Hello from React ES6 {this.props.phrase}!</h1>;
+        return <h1>Hello from React {this.props.phrase}!</h1>;
     }
 }
 
