b07dec402c1d4c08324a395658f75033eaf81e17
Richard Schneiderman <schneiderman.richard@yahoo.com>
2017-04-27 23:10:57 -0400

    add local font support

--- webpack.config.js
+++ webpack.config.js
@@ -24,11 +24,32 @@ module.exports = {
                 test: /\.css/,
                 loaders: ['style', 'css'],
                 include: path.resolve(__dirname,"src")
+             },
+             {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+             }, 
+             {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+             }, 
+             {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+             },
+             {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+             }, 
+             {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
              }
          ]
      },
      devServer: {
         inline : true,
-        port : 3033
+        port : 3033,
+        disableHostCheck: true
      }
  }
